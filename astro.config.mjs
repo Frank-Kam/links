aa4c94e5de45bf5b53c9d8435546801582cce3cd
Frank-Kam <ganl68384@gmail.com>
2025-11-16 19:16:28 +0800

    edit site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig, sharpImageService } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://frankk.uk',
+  site: 'https://frk.tw',
   image: {
     service: sharpImageService(),
   },
